6553ad1c1acdbf033176178221a46244d1e0ddc0
gayu@example.com <gayu@example.com>
2022-04-26 12:51:42 -0700

    1st commit

---
+++ components/Map.js
@@ -0,0 +1,44 @@
+import { useEffect, useContext } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { UberContext } from '../context/uberContext'
+
+const style = {
+  wrapper: `flex-1 h-full w-full`,
+}
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2F5YTNzYWRoYW5hIiwiYSI6ImNsMXZzMjh0eDA5NG8za3BpeDRvMjYydG8ifQ.wFZF3tGhUEMHgZsNdui_2g';
+
+const Map = () => {
+  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
+
+    if (pickupCoordinates) {
+      addToMap(map, pickupCoordinates)
+    }
+
+    if (dropoffCoordinates) {
+      addToMap(map, dropoffCoordinates)
+    }
+
+    if (pickupCoordinates && dropoffCoordinates) {
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 400,
+      })
+    }
+  }, [pickupCoordinates, dropoffCoordinates])
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+  }
+
+  return <div className={style.wrapper} id='map' />
+}
+
+export default Map
